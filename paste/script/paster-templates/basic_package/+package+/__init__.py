6ea27f0c6303afd169447579dc2d1c4d25b9e827
tomswalllow <it27tsw@st-aidans.lancs.sch.uk>
2025-05-16 09:26:11 +0100

    Update __init__.py

--- paste/script/paster-templates/basic_package/+package+/__init__.py
+++ paste/script/paster-templates/basic_package/+package+/__init__.py
@@ -1,1 +1,19 @@
-#
+#' Get the Repository object
+dim repository
+set repository = CreateObject("EA.Repository")
+
+' Get the current diagram
+dim currentDiagram
+set currentDiagram = repository.GetCurrentDiagram()
+
+' Check if a diagram is open
+if not currentDiagram is nothing then
+' Activate the diagram tab
+repository.ShowInProjectView currentDiagram
+else
+' No diagram is open, handle accordingly
+Session.Output "No diagram is currently open."
+end if
+
+' Release the Repository object
+set repository = nothing
